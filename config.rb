082c69aecd3957c518805a1a3b90def12000fffc
Josef Zacek <info@josefzacek.com>
2016-08-10 22:17:09 +0100

    activate :minify_css

--- config.rb
+++ config.rb
@@ -37,7 +37,7 @@ page '/*.txt', layout: false
 # Build-specific configuration
 configure :build do
   # Minify CSS on build
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
   # activate :minify_javascript

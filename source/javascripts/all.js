33946eb251766ffebe31bbd797790102014ac79b
Josef Zacek <info@josefzacek.com>
2016-08-13 19:35:38 +0100

    contact map zoom closer

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,4 +1,4 @@
-var map = L.map('map_holder').setView([53.3498, -6.2603], 11);
+var map = L.map('map_holder').setView([53.3498, -6.2603], 15);
 
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'

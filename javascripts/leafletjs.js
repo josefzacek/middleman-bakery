f4ee4ffc8bbbc2a72eceeda174fad5c227c7f0c1
Josef Zacek <info@josefzacek.com>
2016-08-27 20:20:42 +0100

    Automated commit at 2016-08-27 19:20:42 UTC by middleman-deploy 2.0.0-alpha

---
+++ javascripts/leafletjs.js
@@ -0,0 +1,1 @@
+var map=L.map("map_holder").setView([53.349729,-6.260631],15);L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png",{attribution:'&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'}).addTo(map),L.marker([53.349729,-6.260631]).addTo(map).bindPopup("Your local Bakery"),map.dragging.disable(),map.scrollWheelZoom.disable();

4aa85ac88529ce4504212594d630910991d31a47
Josef Zacek <info@josefzacek.com>
2016-09-02 22:12:32 +0100

    Automated commit at 2016-09-02 21:12:32 UTC by middleman-deploy 2.0.0-alpha

--- javascripts/all.js
+++ javascripts/all.js
@@ -1,1 +1,1 @@
-jQuery(document).ready(function(e){var t=e("#nav-toggle"),a=e("#nav-menu");t.click(function(){e(this).toggleClass("is-active"),a.toggleClass("is-active")})});
+$(document).ready(function(){$(".owl-carousel").owlCarousel({slideSpeed:300,autoPlay:3e3,singleItem:!0})});
